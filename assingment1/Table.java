bb10be1cf7b13b5f65858fce156d455c7f6d18f0
Deepika Singh <71891816+Deepika-Singh111@users.noreply.github.com>
2020-10-06 17:27:52 +0530

    Update Table.java
    I have changes code according to your request.
    Please check it out.

--- assingment1/Table.java
+++ assingment1/Table.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Tables {
 
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
+		try(Scanner sc=new Scanner(System.in)){
 		
 		System.out.println("Enter the number for finding table of the number: ");
 		
@@ -12,10 +12,9 @@ public class Tables {
 		
 		//for loop for finding the table of the given number
 		for(int j=1; j<=10; j++) {
-			int result=n*j;
-			System.out.print(result+ " ");
+			int result = n*j;
+			System.out.print(n + " * " + j + " = " + result+ " ");
 		}
-			
+		}		
 	}
-
 }
