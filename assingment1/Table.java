6b044708df8a5aca69c3c81a4e5480991688fe65
Deepika Singh <71891816+Deepika-Singh111@users.noreply.github.com>
2020-10-06 17:36:33 +0530

    Update Table.java

--- assingment1/Table.java
+++ assingment1/Table.java
@@ -6,15 +6,15 @@ public class Tables {
 	public static void main(String[] args) {
 		try(Scanner sc=new Scanner(System.in)){
 		
-		System.out.println("Enter the number for finding table of the number: ");
-		
-		int n = sc.nextInt();
-		
-		//for loop for finding the table of the given number
-		for(int j=1; j<=10; j++) {
-			int result = n*j;
-			System.out.print(n + " * " + j + " = " + result+ " ");
-		}
+			System.out.println("Enter the number for finding table of the number: ");
+
+			int n = sc.nextInt();
+
+			//for loop for finding the table of the given number
+			for(int j=1; j<=10; j++) {
+				int result = n*j;
+				System.out.print(n + " * " + j + " = " + result+ " ");
+			}
 		}		
 	}
 }
